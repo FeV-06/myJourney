bb2b616938c762c1fb9020e02375b8d2a385503e
FeV-06 <fev.gjulka@gmail.com>
2025-07-15 20:24:16 +0530

    day 4

--- vector.cpp
+++ vector.cpp
@@ -8,6 +8,35 @@ void traverseArray(const std::vector<int> &arr){
     std::cout << std::endl;
 }
 
+
+int linearSearchVec(std::vector<int> &vec, int target){
+    for(int i = 0; i < vec.size(); i++){
+        if(target == vec[i]) return i;
+    }
+    return -1;
+}
+
+int binarySearchVec(std::vector<int> &vec, int target){
+    int st = 0, end = vec.size() - 1, mid = st + (end - st) / 2;
+    
+    while(st <= end){
+        mid = st + (end - st) / 2;
+        if(target == vec[mid]) return mid;
+        if(target > vec[mid]) st = mid + 1;
+        else end = mid - 1;
+    }
+    return -1;
+}
+
+void takeInputVec(std::vector<int> &vec, int input = 0){
+    while(true){
+        std::cout << "Enter a number (-1 to show the complete vector): ";
+        std::cin >> input;                        // loop for vector inputs (-1 will exit the loop)
+        if(input == -1) break;
+        vec.push_back(input);
+    }
+}
+
 void traverseVector(const std::vector<int> &vec){
     for(int x : vec){
         std::cout << x << " ";
@@ -18,7 +47,7 @@ void traverseVector(const std::vector<int> &vec){
 int main()
 {   
     // initializing important datatypes
-    int size, input, count = 0;
+    int size, target, count = 0;
     std::vector<int> vec = {};
 
     // taking input for array
@@ -40,12 +69,12 @@ int main()
     traverseArray(arr);   // prints output array to the console
     std::cout << std::endl;
 
-    while(true){
-        std::cout << "Enter a number (-1 to show the complete vector): ";
-        std::cin >> input;                        // loop for vector inputs (-1 will exit the loop)
-        if(input == -1) break;
-        vec.push_back(input);
-    }
+    takeInputVec(vec);
     traverseVector(vec);    // prints output vector to the console
+
+    std::cout << "Enter the number you want to find: ";
+    std::cin >> target;
+    std::cout << std::endl << "The index of the " << target << " you want to find is: " << linearSearchVec(vec, target);
+    
     return 0;
 }
